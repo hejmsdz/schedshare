2398136888b36d8ffabdbd0f1e32e5b78d608685
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2018-10-27 01:23:32 +0200

    seed the database with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create [
+  127347, 127345, 127341, 127337, 127334, 127323, 127317, 127303, 127296,
+  127295, 127292, 127281, 127277, 127272, 127270, 127268, 127259, 127249,
+  127248, 127236, 127229, 127224, 127214, 127206, 127205, 127196, 126870
+].map { |id| { id: id, email: "s#{id}@student.put.poznan.pl" }}
