b351fb14100d8464adecbbb6f02e659dfd5e7dcf
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2018-10-27 01:08:55 +0200

    send token by email

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -2,6 +2,13 @@ class TokensController < Knock::AuthTokenController
   skip_forgery_protection
   skip_before_action :authenticate
 
+  def create
+    UserMailer.token(entity, auth_token.token).deliver_later
+    head :no_content
+  end
+
+  private
+
   def entity_class
     User
   end
--- spec/controllers/tokens_controller_spec.rb
+++ spec/controllers/tokens_controller_spec.rb
@@ -4,11 +4,16 @@ RSpec.describe TokensController, type: :controller do
   let(:user) { create(:user) }
 
   describe '#create' do
+    let(:deliveries) { ActionMailer::Base.deliveries }
     let(:action) { post :create, params: { user_id: user.id } }
 
-    it 'returns a valid access token' do
+    it 'sends the token by email to the user' do
       action
-      token = Knock::AuthToken.new(token: json_response['jwt'])
+
+      expect(deliveries.length).to eq(1)
+      expect(deliveries.first.to).to eq([user.email])
+
+      token = Knock::AuthToken.new(token: deliveries.first.body.to_s.lines[5])
       expect(token.entity_for(user.class)).to eq(user)
     end
   end
